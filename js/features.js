878d5b75c41f1c790eb2e925fe03b5a8c87490e3
Alvaro Garcia <alvvdc@gmail.com>
2019-08-04 22:56:18 +0200

    Done function for list load originators and their rating from current and finished investments. 	modified:   js/features.js

--- js/features.js
+++ js/features.js
@@ -1,5 +1,6 @@
 const LOAN_ORIGINATOR = "Loan Originator";
 const REMAINING_TERM = "Remaining Term";
+const MINTOS_RATINGS = "Mintos Ratings";
 
 // List load originators from current and finished investments. It does not show repeated values.
 function listAllLoanOriginators()
@@ -21,6 +22,52 @@ function listAllLoanOriginators()
     }
 
     return result.sort();
+}
+
+// List load originators and their rating from current and finished investments. It does not show repeated values.
+function listAllLoanOriginatorsWithRating()
+{
+    let currentOriginators = new Array();
+    let finishedOriginators = new Array();
+    let result = new Array();
+
+    for (let i=0; i<currentInvestments.length; i++)
+    {
+        let originator = new Array();
+        originator.push(currentInvestments[i][getColumnPositionFromHeader(LOAN_ORIGINATOR, currentInvestmentsHeader)]);
+        originator.push(currentInvestments[i][getColumnPositionFromHeader(MINTOS_RATINGS, currentInvestmentsHeader)]);
+        currentOriginators.push(originator);
+    }
+
+    for (let i=0; i<finishedInvestments.length; i++)
+    {
+        let originator = new Array();
+        originator.push(finishedInvestments[i][getColumnPositionFromHeader(LOAN_ORIGINATOR, finishedInvestmentsHeader)]);
+        originator.push(finishedInvestments[i][getColumnPositionFromHeader(MINTOS_RATINGS, finishedInvestmentsHeader)]);
+        finishedOriginators.push(originator);
+    }
+
+    let includedOriginatorsName = new Array();
+
+    for (let i=0; i<currentOriginators.length; i++)
+    {       
+        if (!includedOriginatorsName.includes(currentOriginators[i][0]))
+        {
+            result.push(currentOriginators[i]);
+            includedOriginatorsName.push(currentOriginators[i][0]);
+        }
+    }
+
+    for (let i=0; i<finishedOriginators.length; i++)
+    {
+        if (!includedOriginatorsName.includes(finishedOriginators[i][0]))
+        {
+            result.push(finishedOriginators[i]);
+            includedOriginatorsName.push(finishedOriginators[i][0]);
+        }
+    }    
+
+    return result[1].sort();
 }
 
 function listCurrentInvestmentsAreLate()
