ecb75dc33f83c7e8ac9a9a6eebed2cdaff588b3a
Alvaro Garcia <alvvdc@gmail.com>
2019-08-05 13:16:30 +0200

    Improved table for all loan originators invested in. 	modified:   index.html 	modified:   js/view.js

--- js/view.js
+++ js/view.js
@@ -1,11 +1,16 @@
 
 
-function generateTable() {
+function generateTableForAllLoanOriginatorsInvestedIn() {
     let investmentsLoanOriginators = listAllLoanOriginatorsWithRating();
+
+    $("<h5>").html("Todos los originadores de préstamos con los que has invertido").appendTo("#investmentsLoanOriginatorsTable");
+        let table = $("<table>").addClass("table").appendTo("#investmentsLoanOriginatorsTable");
+        $("<thead>").html("<tr><th>Nombre</th><th>Evaluación</th></tr>").appendTo(table);
+        let tbody = $("<tbody>").appendTo(table);
 
     for (let i=0; i<investmentsLoanOriginators.length; i++)
     {
-        let row = $("<tr>").appendTo("#investmentsLoanOriginatorsTableBody");
+        let row = $("<tr>").appendTo(tbody);
         $("<td>").html(investmentsLoanOriginators[i][0]).appendTo(row);
         let rate = $("<td>").html("<span>" + investmentsLoanOriginators[i][1] + "</span>").appendTo(row);
 
@@ -28,5 +33,4 @@ function generateTable() {
                 break;
         }
     }
-    console.log("ejecutado");
 }
